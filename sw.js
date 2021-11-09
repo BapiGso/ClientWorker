2ba51d624d28682154198f22f78cd104ce25e8f4
BapiGso <59081856+BapiGso@users.noreply.github.com>
2021-11-09 20:49:00 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -90,10 +90,7 @@ const custom = async (req) => {
 
     
     if (n.status >= 400 || n === "") {
-        return new Response(    if (n.status >= 400 || n === "") {
-        return new Response(`<h1>ClientWorker服务端错误</h2>
-        <b>错误代码：${n.status}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
-    }`, { headers: { "content-type": "text/html; charset=utf-8" } })
+        return new Response(`<!DOCTYPE html><html lang="zh-CN" class=""><head><title>晓梦  | 页面没找到</title><link rel="stylesheet" href="/usr/themes/Diaspora-Typecho-master/assets/css/Diaspora.css"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta charset="UTF-8"><meta name="apple-mobile-web-app-capable" content="yes" /><meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"><meta name="apple-mobile-web-app-capable" content="yes" /><meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"><meta name="google" content="notranslate" /><head><div id="single" class="page"><div id="top"><a class="image-icon" href="javascript:history.back()"></a></div><div class="section">	<div class="images">	</div><div class="article"><div><div class="content"><h1>404 Not Found</h1><p>The page you were looking for is no longer available.</p></div></div></div></div></div></body></html>`, { headers: { "content-type": "text/html; charset=utf-8" } })
     }
     else {
         return n
